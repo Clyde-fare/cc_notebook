c3ede169a8422e70fc678269d9687ec35d52b36f
Clyde-fare <clyde.fare@gmail.com>
2014-12-11 20:19:04 +0000

    Updated to reflect changes in project structure

--- custom/cc_notebook.js
+++ custom/cc_notebook.js
@@ -2,16 +2,16 @@
 IPython.cc_notebook = {}
 IPython.cc_notebook.view_function = function (event, file_n) {
     if (event.ctrlKey && event.shiftKey){
-        IPython.notebook.kernel.execute("import cc_notebook_utils; cc_notebook_utils.pygview('" + file_n + ".log')");
+        IPython.notebook.kernel.execute("from cc_notebook import pygview; pygview('" + file_n + ".log')");
     }
     else if (event.ctrlKey){
-        IPython.notebook.kernel.execute("import cc_notebook_utils; cc_notebook_utils.pygausssum('" + file_n + ".log')");
+        IPython.notebook.kernel.execute("from cc_notebook import pygausssum; pygausssum('" + file_n + ".log')");
     }
     else if (event.shiftKey){
-        IPython.notebook.kernel.execute("import cc_notebook_utils; cc_notebook_utils.pyvogadro('" + file_n + ".log')");
+        IPython.notebook.kernel.execute("from cc_notebook import pyvogadro; pyvogadro('" + file_n + ".log')");
     }
     else {
-        IPython.notebook.kernel.execute("import cc_notebook_utils; cc_notebook_utils.pyvim('" + file_n + ".log')");
+        IPython.notebook.kernel.execute("from cc_notebook import pyvim; pyvim('" + file_n + ".log')");
         IPython.notebook.kernel.execute("print(file_n)");
     }
 }
