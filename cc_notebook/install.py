d396c145d96da6717a247fd5ccdffc303dd70c83
Clyde-fare <clyde.fare@gmail.com>
2015-01-28 22:01:54 +0000

    Created an exit point cc_notebook_init used to create the .cc_notebook.ini files and place them in the home directory.

--- cc_notebook/install.py
+++ cc_notebook/install.py
@@ -4,6 +4,7 @@ import os
 import warnings
 from IPython.display import display, Javascript
 from IPython.html.nbextensions import install_nbextension
+
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     from pkg_resources import resource_filename
@@ -15,16 +16,58 @@ require(['nbextensions/cc_notebook'])
 require(['nbextensions/jsmol/JSmol.min.nojq'])
 ''')
 
+
+def cc_notebook_init(verbose=0):
+    """Initilises cc_notebook
+
+    Checks that .cc_notebook.ini exists on the home directory, if not places an empty one there
+
+    Installs javascript dependencies"""
+
+    default_cc_ini = ( "[pbs]",
+                       "user=username",
+                       "server=servername",
+                       "[gaussian]",
+                       "gauss_host=user@server",
+                       "gauss_home=remote_homedirectory",
+                       "gauss_scratch=remote_scratchdirectory",
+                       "[ase]",
+                       "ase_home=local_homedirectory",
+                       "ase_scratch=local_scratchdirectory",
+                       "[smart logs]",
+                       "click=open -e",
+                       "shift_click=prog1",
+                       "cntrl_click=prog2",
+                       "cntrl_shift_click=prog3" )
+
+    cc_ini_fl_pth = os.path.expanduser('~/.cc_notebook.ini')
+
+    if not os.path.isfile(cc_ini_fl_pth):
+        with open(cc_ini_fl_pth, 'w') as cc_ini:
+            cc_ini.write('\n'.join(default_cc_ini))
+
+        print('\n{f} constructed please fill in details\n'.format(f=cc_ini_fl_pth))
+
+    else:
+        print('\n{f} already exists not overwriting\n'.format(f=cc_ini_fl_pth))
+
 def enable_notebook(verbose=0):
-    """Enable IPython notebook widgets to be displayed.
+    """Enables notebook widgets to be displayed.
 
     This function should be called before using cc_notebook.
     """
-    libs = ['cc_notebook.js', 
+    cc_ini_fl_pth = os.path.expanduser('~/.cc_notebook.ini')
+    if not os.path.isfile(cc_ini_fl_pth):
+        raise(RuntimeError('cc_notebook not initialised please run cc_notebook_init from the terminal'))
+
+    libs = ['cc_notebook.js',
             'jsmol']
     fns = [resource_filename('cc_notebook', os.path.join('static', f)) for f in libs]
     install_nbextension([fns[0]], verbose=verbose, overwrite=True)
     
-    #jsmol is large so we won't overwrite
+    # jsmol is large so we won't overwrite
     install_nbextension(fns[1:], verbose=verbose, overwrite=False)
     display(activate_nb)
+
+if __name__ == '__main__':
+    cc_notebook_init()
--- setup.py
+++ setup.py
@@ -3,10 +3,19 @@
 import os
 from setuptools import setup
 
+#get static files we want to install
+# (includes multed nested folders within jsmol so rather than list them manually we traverse the directory)
 my_data = [(x[0], [x[0]+'/'+f for f in x[2]]) for x in os.walk('cc_notebook/static')]
 
 setup(name='cc_notebook',
       version='0.1',
       packages=['cc_notebook'],
-      data_files = my_data
+      data_files = my_data,
+
+      entry_points = {
+        'console_scripts': [
+            'cc_notebook_init = cc_notebook.install:cc_notebook_init',
+        ],
+        }
       )
+
